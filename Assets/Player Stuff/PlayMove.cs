f0791b7fe3afebc0d944cf0550a43643c1c9c4a2
Creese54 <c00408978@louisiana.edu>
2022-01-24 18:28:02 -0600

    Basic Dash/Broken Recharge
    Implemented basic dash into player controller, but the recharge does not work when the time is set above 0.

--- Assets/Player Stuff/PlayMove.cs
+++ Assets/Player Stuff/PlayMove.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayMove : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 5f; //12f;
+    public float speed = 8f; //12f;
+    private float Ospeed;
     public float gravity = -9.81f;
     public Vector3 velocity;
     public float jumpHeight = 3f;
@@ -14,14 +15,24 @@ public class PlayMove : MonoBehaviour
     public float dropDist = 0.4f;
     public LayerMask groundMask;
 
+    public float DashSpeed;
+    public float DashTime;
+    public float DashRecharge;
+    float dashTime;
+
     public Transform body;
     bool onGround;
     bool isCrouched = false;
+    bool DashReady = true;
+
+    Vector3 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Ospeed = speed;
+        dashTime = DashTime / 10f;
+        //DashReady = true;
     }
 
     // Update is called once per frame
@@ -36,31 +47,92 @@ public class PlayMove : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        movement = transform.right * x + transform.forward * z;
+        /*
         // Sprint: 
         if (onGround && isCrouched == false){
             if (Input.GetKey(KeyCode.LeftShift)) {
-                speed = 12f;
+                speed = Ospeed*1.5f;
             } else {
-                speed = 8f;
+                speed = Ospeed; //8f;
             }
-        }
+        } */
         //Crouch:
         if (Input.GetKeyDown(KeyCode.C)) {
-            speed = 3f;
+            speed = Ospeed*0.375f;
             body.localScale = new Vector3(1f,.50f,1f);
             isCrouched = true;
         } if (Input.GetKeyUp(KeyCode.C)) {
-            speed = 8f;
+            speed = Ospeed; //8f;
             body.localScale = new Vector3(1f,1f,1f);
-            isCrouched = false;
+            isCrouched = false; 
             controller.Move(Vector3.up * .2f);
         }
 
-        Vector3 movement = transform.right * x + transform.forward * z;
-        controller.Move(movement.normalized * speed * Time.deltaTime);
+        //Dash
+        if (DashReady && Input.GetKey(KeyCode.LeftShift))
+        {
+            StartCoroutine(Dash());
+            StartCoroutine(DashReadying());
+        }
+
+        //if (/*onGround &&*/ DashReady == true)
+        /*{
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                StartCoroutine(Dash());
+            }
+            else
+            {
+                speed = Ospeed; //8f;
+            }
+        } */
+
+        //Executing movement
+        //Vector3 movement = transform.right * x + transform.forward * z;
+        controller.Move(movement /*movement.normalized*/ * speed * Time.deltaTime);
         if(Input.GetButtonDown("Jump") && onGround) { velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    IEnumerator Dash()
+    {
+        DashReady = false;
+
+        float startTime = Time.time;
+
+        while (Time.time < startTime + dashTime)
+        {
+            controller.Move(movement * DashSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+
+    }
+
+    IEnumerator DashReadying()
+    {
+        yield return new WaitForSeconds(DashRecharge);
+        DashReady = true;
+
+        /*
+        while (RepulseTimer != 1)
+        {
+            RepulseTimer += .01f;
+            yield return new WaitForSeconds(.01f);
+        }
+        */
+    }
+
+    public bool GetDashReady()
+    {
+        return DashReady;
+    }
+
+    public float GetDashRecharge()
+    {
+        return DashRecharge / 100;
     }
 
     public bool GetOnGround()
