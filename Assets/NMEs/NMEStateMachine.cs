c2063e99f972b3e881b1bf8a949e21ea12442e90
C00414139-ULL <91398946+C00414139-ULL@users.noreply.github.com>
2022-02-01 20:20:49 -0600

    state machine sort of working

--- Assets/NMEs/NMEStateMachine.cs
+++ Assets/NMEs/NMEStateMachine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 /*
 enemy state machine with 3 states.
@@ -13,18 +14,47 @@ enemy state machine with 3 states.
 [3>2] special conditional for Boba to stop jetpacking, probably gonna be a timer so he dosen't just stay up there for too long.
 */
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class NMEStateMachine : MonoBehaviour
 {
     public enum State { Idle, KillLuke, BobaGoWeeInDaSky}
     public State CurState;
 
+    [SerializeField] private Transform Plyr;
+    private UnityEngine.AI.NavMeshAgent AI;
+    private NavMeshAgent NavAg;
+
     private Dictionary<State, System.Action> enter;
     private Dictionary<State, System.Action> exit;
     private Dictionary<State, System.Action> execute;
 
+    float temp;
+    //Idle variables
+    bool isIdle;
+    Vector3 IPos;
+    Vector3 IRot;
+
+    //KillLuke variables
+    [Tooltip("175 for troopers, 255 for boba")]
+    public float VisRng;
+    [Tooltip("distance to see luke, probably 25")]
+    public float VisDist;
+
+    Vector3 KPos;
+
+    //bobaGoWeeInDaSky variables
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Plyr = GameObject.FindGameObjectWithTag("Player").transform;
+        AI = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        isIdle = true;
+        IPos = transform.position;
+        IRot = transform.forward;
+
         enter = new Dictionary<State, System.Action>()
         {
             {State.Idle, entI },
@@ -44,12 +74,25 @@ public class NMEStateMachine : MonoBehaviour
             {State.BobaGoWeeInDaSky, excB }
         };
 
+        CurState = State.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
         execute[CurState]();
+    }
+
+    private bool CanSee()
+    {
+        Vector3 toPlyr = Plyr.position - transform.position;
+        float AglToPlyr = Vector3.Angle(transform.forward, toPlyr);
+        return AglToPlyr < VisRng && Vector3.Distance(transform.position, Plyr.position) < 30;
+    }
+
+    private bool InRange()
+    {
+        return Vector3.Distance(transform.position, Plyr.position) < VisDist;
     }
 
     void Transition(State nextState)
@@ -60,62 +103,100 @@ public class NMEStateMachine : MonoBehaviour
     }
 
     //State Behaviors
-    //Idle: Alpha1
+    //Idle: q
     void entI()
     {
-        Debug.Log("enter Idle");
+
     }
     void extI()
     {
-        Debug.Log("exit Idle");
+        isIdle = false;
     }
     void excI()
     {
-        Debug.Log("execute Idle");
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        //if not there, return to idle possition, then stop checking the if statments
+        if (!isIdle)
+        {
+            if (transform.position != IPos) { AI.destination = IPos; }
+            else { 
+                transform.rotation = Quaternion.LookRotation(IRot);
+                isIdle = true;
+            }
+        }
+        
+        Debug.Log("execute Idle");//change to can see luke later
+        if (Input.GetKey(KeyCode.W))
         {
             Transition(State.KillLuke);
         }
     }
 
-    //KillLuke: Alpha2
+    //KillLuke: w
     void entK()
     {
-        Debug.Log("enter KillLuke");
+
     }
     void extK()
     {
-        Debug.Log("exit KillLuke");
+
     }
     void excK()
     {
+        //circle luke
+        if (Time.realtimeSinceStartup % 2 == 0)//every 2 seconds the position to 'circle' around luke will update
+        {
+            temp = Random.Range(-1, 1);
+            KPos = (Plyr.transform.position + (3 * Plyr.transform.forward)) + new Vector3(temp, Mathf.Sqrt(1- Mathf.Pow(temp, 2)), 0);
+            AI.destination = KPos;
+        }
+        //shoot luke
+        transform.LookAt(Plyr.transform);
+        //shoot look here
+
         Debug.Log("execute KillLuke");
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKey(KeyCode.Q))//replace with luke being out of signt
         {
             Transition(State.Idle);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKey(KeyCode.E))//only have boba use this
         {
             Transition(State.BobaGoWeeInDaSky);
         }
     }
 
-    //BobaGoWeeInDaSky: Alpha3
+    //BobaGoWeeInDaSky: e
     void entB()
     {
-        Debug.Log("enter BobaGoWeeInDaSky");
+
     }
     void extB()
     {
-        Debug.Log("exit BobaGoWeeInDaSky");
+
     }
     void excB()
     {
+        //big jetpack jump
+        StartCoroutine(JetpackJump());    
+
         Debug.Log("execute BobaGoWeeInDaSky");
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKey(KeyCode.W))
         {
             Transition(State.KillLuke);
         }
+    }
+
+    IEnumerator JetpackJump()
+    {
+        temp = 0;
+        while(temp < 1.7)
+        {
+            transform.position += new Vector3(0, 0, .01f);
+            temp += .01f;
+        }
+        temp = 0;
+        //Transition(State.KillLuke);
+
+        yield return null;
     }
 
 
